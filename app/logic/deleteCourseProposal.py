36c37491792d50d0d6cb40025c209b297ddab108
sreynitkhatt <khatts@berea.edu>
2021-08-25 12:30:00 -0400

    created test for deletCourseProposal

--- app/logic/deleteCourseProposal.py
+++ app/logic/deleteCourseProposal.py
@@ -1,17 +1,12 @@
 from app.models.course import Course
 def deleteCourseProposal(courseID):
     courseID = Course.get_by_id(courseID)
-    print("Course ID",courseID)
     courses = Course.select()
 
-    print(list(courses))
     for course in courses:
-        print(type(course), "type of course")
-        print(course.courseName)
-        print("course ID",type(course.id),type(courseID.id))
-        print("Course Status ID",type(course.status_id))
-
         if course.id == courseID.id and course.status_id == 2:
             print("Inside the for loop ")
             course.delete_instance()
             print("deletion was successul")
+        else:
+            print("Not this time")
--- tests/code/test_deleteCourseProposal.py
+++ tests/code/test_deleteCourseProposal.py
@@ -1,1 +1,11 @@
+import pytest
+from peewee import DoesNotExist
 from app.logic.deleteCourseProposal import deleteCourseProposal
+
+
+@pytest.mark.integration
+def test_deleteCourseProposal():
+
+    deleteCourse = deleteCourseProposal(2)
+
+    assert deleteCourse
